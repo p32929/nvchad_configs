1890ee160f68104fd8e653376f873aa10bcca115
p32929 <p32929@yahoo.com>
2022-11-18 21:05:09 +0600

    coc

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,8 +1,8 @@
 return {
     ["ggandor/lightspeed.nvim"] = {},
-    -- ["neoclide/coc.nvim"] = {
-    --     -- branch = 'master',
-    --     tag = 'v0.0.79',
-    --     run = 'yarn install --frozen-lockfile'
-    -- },
+    ["neoclide/coc.nvim"] = {
+        --     -- branch = 'master',
+        tag = 'v0.0.79',
+        --     run = 'yarn install --frozen-lockfile'
+    },
 }
