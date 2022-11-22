6ae729a5dd65a364aaa2922f7a720580219d9c0d
p32929 <p32929@yahoo.com>
2022-11-22 09:22:44 +0600

    changed branch

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,8 +1,6 @@
 return {
     ["ggandor/lightspeed.nvim"] = {},
     ["neoclide/coc.nvim"] = {
-        --     -- branch = 'master',
-        tag = 'v0.0.79',
-        --     run = 'yarn install --frozen-lockfile'
+        branch = 'master',
     },
 }
